a3aadd98cd5c926b673c0a3ce164f6d9076c97aa
Francisco J Soto <fsoto@xinaptic.com>
2013-07-26 14:51:45 -0700

    basic python version

---
+++ Ebolve.py
@@ -0,0 +1,46 @@
+class Ebolve:
+  """"  EBOLVE (EDWARDS-BELL-OHLSON aLghorithmic Value Engine"""
+  
+  def __init__(self, input):
+    self.eps1 = input['eps1']                    # forecasted eps Year 1
+    self.eps2 = input['eps2']                    # forecasted eps Year 2, not neg
+    self.ltg = input['ltg']                      # contant rate of eps growth, format 0.25
+    self.book_s = input['book_s']                # starting book value
+    self.FROE = input['FROE']                    # long term equilibrium ROE, not neg
+    self.r = input['r']                          # discount rate, format 0.08
+    self.retain = 1.0 - input['pYOUT']           # % dividend payout, format 0.16
+    self.years = input['years']                  # total years horizon, i.e. 12
+    self.growth_years = input['growth_years']    # years of LTG, i.e. 5
+    #self.ebo = compute_ebo()
+    
+  def compute_ebo(self):
+    eps, roe, abn_eps, cum_ab_eps, eps_g = 0.0, 0.0, 0.0, 0.0, 0.0
+    bv = self.book_s
+    roe_step = 0
+
+    in_eps = [self.eps1, self.eps2]
+    for i in range(self.growth_years):
+      in_eps.append(in_eps[-1]*(1 + self.ltg))
+    
+    for year in range(self.years):
+      if year <= 1 + self.growth_years:
+        bv += eps * self.retain
+        eps = in_eps[year]
+        roe = eps / bv
+      else:
+        if roe_step == 0:
+          roe_step = (self.FROE - roe) / (self.years - self.growth_years - 2)
+        bv *= 1 + (roe * self.retain)
+        roe += roe_step
+        eps_g = eps
+        eps = bv * roe
+        eps_g = (eps-eps_g)/eps_g
+        print eps
+      
+      abn_eps = (roe - self.r) * bv
+      cum_ab_eps += abn_eps / ((1 + self.r)**(year+1))
+    
+    terminal_Value = ((self.FROE - self.r) * bv) / (self.r * (1 + self.r)**self.years)
+    
+    return (self.book_s + cum_ab_eps + terminal_Value)
+	
