16e64f3079e26890024fa5c5175f138e5b92f5f0
Javier Soto <sotoseattle@gmail.com>
2014-08-20 08:18:00 -0700

    Apply POODR principle: Hide instance variables (private access).

--- Ebolve.rb
+++ Ebolve.rb
@@ -7,14 +7,19 @@ require 'rubygems'
 ###################################################
 
 class Ebolve
-  attr_reader :ebo
   
+  private
+  attr_reader :eps1, :eps2, :ltg, :book_s, :fROE, :r, 
+              :retain, :years, :growth_years
+  public
+  attr_reader :ebo
+
   def initialize (input)
     @eps1 = input[:eps1]                    # forecasted eps Year 1
     @eps2 = input[:eps2]                    # forecasted eps Year 2, not neg
     @ltg = input[:ltg]                      # contant rate of eps growth, format 0.25
     @book_s = input[:book_s]                # starting book value
-    @FROE = input[:FROE]                    # long term equilibrium ROE, not neg
+    @fROE = input[:FROE]                    # long term equilibrium ROE, not neg
     @r = input[:r]                          # discount rate, format 0.08
     @retain = 1.0 - input[:pYOUT]           # % dividend payout, format 0.16
     @years = input[:years]                  # total years horizon, i.e. 12
@@ -24,20 +29,20 @@ class Ebolve
 
   def compute_ebo
     eps, roe, abn_eps, cum_ab_eps, eps_g = 0.0, 0.0, 0.0, 0.0, 0.0
-    bv = @book_s
+    bv = book_s
     roe_step = nil
     
-    in_eps = [@eps1, @eps2]
-    @growth_years.times{|i| in_eps << in_eps.last*(1 + @ltg)}
+    in_eps = [eps1, eps2]
+    growth_years.times{|i| in_eps << in_eps.last*(1 + ltg)}
     
-    (0..@years-1).each do |year|
-      if year <= 1 + @growth_years
-        bv += eps * @retain
+    (0..years-1).each do |year|
+      if year <= 1 + growth_years
+        bv += eps * retain
         eps = in_eps[year]
         roe = eps / bv
       else
-        roe_step ||= (@FROE - roe) / (@years - @growth_years - 2)
-        bv *= 1 + (roe * @retain)
+        roe_step ||= (fROE - roe) / (years - growth_years - 2)
+        bv *= 1 + (roe * retain)
         roe += roe_step
 
         eps_g = eps
@@ -45,11 +50,11 @@ class Ebolve
         eps_g = (eps-eps_g)/eps_g
       end
       
-      abn_eps = (roe - @r) * bv
-      cum_ab_eps += abn_eps / ((1 + @r)**(year+1))
+      abn_eps = (roe - r) * bv
+      cum_ab_eps += abn_eps / ((1 + r)**(year+1))
     end
 
-    terminal_Value = ((@FROE - @r) * bv) / (@r * (1 + @r)**@years)
+    terminal_Value = ((fROE - r) * bv) / (r * (1 + r)**years)
 
     warn("WARN: book value is negative: #{bv}") if bv<0.0
     warn("WARN: terminal value is negative: #{terminal_Value}") if terminal_Value<0.0
@@ -57,7 +62,7 @@ class Ebolve
     warn("WARN: negative abnormal earnings in perpetuity: #{abn_eps}") if abn_eps<0.0
     warn("WARN: earnings growing in perpetuity at : #{100*eps_g}%") if (eps_g>0.1 || eps_g<0.06)
 
-    return (@book_s + cum_ab_eps + terminal_Value)
+    return (book_s + cum_ab_eps + terminal_Value)
   end
   private :compute_ebo
 end
